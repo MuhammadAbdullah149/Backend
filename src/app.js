f2f7ce423d5195f3778c356367e2f4d5eeb3a98d
MuhammadAbdullah149 <mabdullah0391@gmail.com>
2026-03-14 10:44:19 +0500

    error handeled

--- src/app.js
+++ src/app.js
@@ -9,5 +9,9 @@ const app = express()
                 origin: process.env.CORS_ORIGIN,
                 credentials:true
         }))
+        app.use(express.json({limit:"16kb"}))
+        app.use(express.urlencoded({extended:true,limit:"16kb"}))
+        app.use(express.static("public"))
+        app.use(cookieParser())
         
 export {app}
